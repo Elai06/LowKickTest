7f5c3434e34f0bc16e7ff2ce83cb1c49171be31f
Elai <dof312@gmail.com>
2023-11-17 18:35:17 +0300

    minor revision

--- Assets/Scripts/Views/AnimationView.cs
+++ Assets/Scripts/Views/AnimationView.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Views
@@ -7,6 +8,9 @@ namespace Views
     {
         [SerializeField] private Animator _animationClip;
         [SerializeField] private Slider _slider;
+        [SerializeField] private GameObject _circle;
+
+        private Tween _tween;
 
         private void OnEnable()
         {
@@ -17,11 +21,21 @@ namespace Views
         private void OnDisable()
         {
             _slider.onValueChanged.RemoveListener(SliderValueChanged);
+            _tween?.Kill();
         }
 
         private void SliderValueChanged(float value)
         {
-             _animationClip.speed = value;
+            //  _animationClip.speed = value;
+            PlayAnimation(value);
+        }
+
+        private void PlayAnimation(float speed)
+        {
+            _tween = DOVirtual.Float(0, 280, speed, value =>
+                {
+                    _circle.transform.localPosition = Vector3.right * value;
+                }).SetSpeedBased(true).SetLoops(-1, LoopType.Yoyo);
         }
     }
 }
